12cd91c8825bb4fe3f234f1947243d20ab1ea8dd
stavros-melidoniotis <melidon.stavros@gmail.com>
2022-08-26 16:33:14 +0300

    Changed font to Poppins

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,8 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
