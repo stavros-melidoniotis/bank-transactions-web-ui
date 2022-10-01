e75a54eff7df0cd6c3bbf58c9f43bd69191757f8
stavros-melidoniotis <melidon.stavros@gmail.com>
2022-10-01 10:57:34 +0300

    Format values using Intl

--- components/TotalBox.jsx
+++ components/TotalBox.jsx
@@ -1,7 +1,14 @@
 const TotalBox = ({ title, value }) => {
     return (
         <div className={`w-full h-32 px-4 py-4 rounded-md relative flex flex-col justify-center items-center glass-bg`}>
-            <span className='text-2xl mb-2'>{value.toFixed(2)} € </span>
+            <span className='text-2xl mb-2'>
+                {
+                    new Intl.NumberFormat('el-GR', { 
+                        style: 'currency', 
+                        currency: 'EUR' }
+                    ).format(value)
+                }
+            </span>
 
             <p className=''> {title} </p>
         </div>
